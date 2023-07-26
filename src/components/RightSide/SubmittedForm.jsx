0d4fa8c33e30e7b29d3014961d29dba2ecf2df43
Jose <jarcila.carbonell@gmail.com>
2023-07-26 17:43:05 -0500

    last try to fix images

--- src/components/RightSide/SubmittedForm.jsx
+++ src/components/RightSide/SubmittedForm.jsx
@@ -1,7 +1,7 @@
 export const SubmittedForm = () => {
   return (
     <div className="ty-page">
-        <img src="./assets/images/icon-complete.svg" alt="" />
+        <img src="./icon-complete.svg" alt="" />
 
         <div className="ty-page-text-container">
             <h1>THANK YOU!</h1>
